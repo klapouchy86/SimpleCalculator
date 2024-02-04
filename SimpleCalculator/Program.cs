e6a9ce6420dd4e7b649be6fd25bf4620e52120ca
klapouchy86 <m.jelski86@gmail.com>
2024-02-04 10:42:23 +0100

    Simple calculator finished

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,7 +10,46 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            float num1 = 0;
+            float num2 = 0;
+
             Console.WriteLine("Hello, welcome to the Simple Calculator Program");
+            Console.WriteLine("Please, enter a first number: ");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please, enter a second number: ");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What type of operation would you like to do?");
+            Console.WriteLine("Please enter 'a' for addition, 's' fot subtraction, 'm' for multiplication or 'd' for division.");
+
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    while (num2 == 0)
+                    {
+                        Console.WriteLine("We DON'T divide by zero!!! Enter diffrent number: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+
+
+            Console.WriteLine("Thank you for cooperation ;-)");
+
+            Console.ReadKey();
         }
     }
 }
